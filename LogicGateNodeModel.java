9b509b608e99028a5f3d8777cd54789e7a9f9e8f
Gökhan Gerdan <gokhang1327@gmail.com>
2020-03-13 15:19:58 +0300

    Update LogicGateNodeModel.java

--- LogicGateNodeModel.java
+++ LogicGateNodeModel.java
@@ -15,6 +15,7 @@ import org.knime.core.data.DataType;
 import org.knime.core.data.container.CloseableRowIterator;
 import org.knime.core.data.def.DefaultRow;
 import org.knime.core.data.def.IntCell;
+import org.knime.core.data.def.IntCell.IntCellFactory;
 import org.knime.core.data.def.StringCell;
 import org.knime.core.node.BufferedDataContainer;
 import org.knime.core.node.BufferedDataTable;
@@ -26,6 +27,7 @@ import org.knime.core.node.NodeLogger;
 import org.knime.core.node.NodeModel;
 import org.knime.core.node.NodeSettingsRO;
 import org.knime.core.node.NodeSettingsWO;
+import org.knime.core.node.defaultnodesettings.SettingsModelColumnName;
 import org.knime.core.node.defaultnodesettings.SettingsModelString;
 
 /**
@@ -43,11 +45,11 @@ public class LogicGateNodeModel extends NodeModel {
 	
 	private static final String KEY_INPUT_A = "input_a";
 	private static final String DEFAULT_INPUT_A = "";
-	private final SettingsModelString m_inputASettings = createInputASettingsModel();
+	private final SettingsModelColumnName m_inputASettings = createInputASettingsModel();
 	
 	private static final String KEY_INPUT_B = "input_b";
 	private static final String DEFAULT_INPUT_B = "";
-	private final SettingsModelString m_inputBSettings = createInputBSettingsModel();
+	private final SettingsModelColumnName m_inputBSettings = createInputBSettingsModel();
     
     /**
      * Constructor for the node model.
@@ -62,12 +64,12 @@ public class LogicGateNodeModel extends NodeModel {
 		return new SettingsModelString(KEY_GATE_TYPE, DEFAULT_GATE_TYPE);
 	}
     
-    static SettingsModelString createInputASettingsModel() {
-		return new SettingsModelString(KEY_INPUT_A, DEFAULT_INPUT_A);
+    static SettingsModelColumnName createInputASettingsModel() {
+		return new SettingsModelColumnName(KEY_INPUT_A, DEFAULT_INPUT_A);
 	}
     
-    static SettingsModelString createInputBSettingsModel() {
-		return new SettingsModelString(KEY_INPUT_B, DEFAULT_INPUT_B);
+    static SettingsModelColumnName createInputBSettingsModel() {
+		return new SettingsModelColumnName(KEY_INPUT_B, DEFAULT_INPUT_B);
 	}
 
     /**
@@ -83,6 +85,13 @@ public class LogicGateNodeModel extends NodeModel {
 		
 		DataTableSpec inputTableSpec = inputTable.getDataTableSpec();
 		
+		String logicGateString = m_logicGateSettings.getStringValue();
+		String inputAString = m_inputASettings.getColumnName();
+		String inputBString = m_inputBSettings.getColumnName();
+		
+		int inputAColumnIndex = inputTableSpec.findColumnIndex(inputAString);
+		int inputBColumnIndex = inputTableSpec.findColumnIndex(inputBString);
+		
 		List<DataColumnSpec> newColumnSpecs = new ArrayList<>();
 		
 		for (int i = 0; i < inputTableSpec.getNumColumns(); i++) {
@@ -92,6 +101,12 @@ public class LogicGateNodeModel extends NodeModel {
 			DataColumnSpecCreator specCreator = new DataColumnSpecCreator(newName, newDataType);
 			newColumnSpecs.add(specCreator.createSpec());
 		}
+		
+		// Create output column
+		String newName = inputAString + "_" + logicGateString + "_" + inputBString;
+		DataType newDataType = IntCellFactory.TYPE;
+		DataColumnSpecCreator specCreator = new DataColumnSpecCreator(newName, newDataType);
+		newColumnSpecs.add(specCreator.createSpec());
 		
 		DataColumnSpec[] newColumnSpecsArray = newColumnSpecs.toArray(new DataColumnSpec[newColumnSpecs.size()]);
 		DataTableSpec outputSpec = new DataTableSpec(newColumnSpecsArray);
@@ -110,6 +125,35 @@ public class LogicGateNodeModel extends NodeModel {
 				DataCell cell = currentRow.getCell(i);
 				cells.add(cell);
 			}
+			
+			int inputAValue = Integer.parseInt(currentRow.getCell(inputAColumnIndex).toString());
+			int inputBValue = Integer.parseInt(currentRow.getCell(inputBColumnIndex).toString());
+			int outputValue = 0;
+			
+			if (logicGateString == "AND") {
+				if ((inputAValue == 1) && (inputBValue == 1)) {
+					outputValue = 1;
+				} else if ((inputAValue == 1) && (inputBValue == 0)) {
+					outputValue = 0;
+				} else if ((inputAValue == 0) && (inputBValue == 0)) {
+					outputValue = 0;
+				} else if ((inputAValue == 0) && (inputBValue == 1)) {
+					outputValue = 0;
+				}
+			} else if (logicGateString == "OR") {
+				if ((inputAValue == 1) && (inputBValue == 1)) {
+					outputValue = 1;
+				} else if ((inputAValue == 1) && (inputBValue == 0)) {
+					outputValue = 1;
+				} else if ((inputAValue == 0) && (inputBValue == 0)) {
+					outputValue = 0;
+				} else if ((inputAValue == 0) && (inputBValue == 1)) {
+					outputValue = 1;
+				}
+			}
+			
+			DataCell outputCell = IntCellFactory.create(outputValue);
+			cells.add(outputCell);
 			
 			DataRow row = new DefaultRow(currentRow.getKey(), cells);
 			container.addRowToTable(row);
@@ -153,6 +197,9 @@ public class LogicGateNodeModel extends NodeModel {
     @Override
     protected void saveSettingsTo(final NodeSettingsWO settings) {
          // TODO: generated method stub
+    	m_logicGateSettings.saveSettingsTo(settings);
+    	m_inputASettings.saveSettingsTo(settings);
+    	m_inputBSettings.saveSettingsTo(settings);
     }
 
     /**
@@ -162,6 +209,9 @@ public class LogicGateNodeModel extends NodeModel {
     protected void loadValidatedSettingsFrom(final NodeSettingsRO settings)
             throws InvalidSettingsException {
         // TODO: generated method stub
+    	m_logicGateSettings.loadSettingsFrom(settings);
+    	m_inputASettings.loadSettingsFrom(settings);
+    	m_inputBSettings.loadSettingsFrom(settings);
     }
 
     /**
@@ -171,6 +221,9 @@ public class LogicGateNodeModel extends NodeModel {
     protected void validateSettings(final NodeSettingsRO settings)
             throws InvalidSettingsException {
         // TODO: generated method stub
+    	m_logicGateSettings.validateSettings(settings);
+    	m_inputASettings.validateSettings(settings);
+    	m_inputBSettings.validateSettings(settings);
     }
     
     /**
